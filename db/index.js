6209866ae85cfd9f13ca0b56c3a5a849ea32e5d4
Jacqueline Callejas <jcr.debaser@gmail.com>
2021-12-13 20:54:03 -0600

    Conexión con base de datos

---
+++ db/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const MONGODB_DATABASE_USERNAME = process.env.MONGODB_DATABASE_USERNAME;
+const MONGODB_DATABASE_PASSWORD = process.env.MONGODB_DATABASE_PASSWORD;
+const MONGODB_DATABASE_HOSTNAME = process.env.MONGODB_DATABASE_HOSTNAME;
+const MONGODB_DATABASE_NAME = process.env.MONGODB_DATABASE_NAME;
+
+const connectionString = `mongodb+srv://${process.env.MONGODB_DATABASE_USERNAME}:${process.env.MONGODB_DATABASE_PASSWORD}@${process.env.MONGODB_DATABASE_HOSTNAME}/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`
+mongoose
+  .connect(connectionString)
+  .then(() => console.log('Conectado a la base de datos'))
+  .catch(err => console.log('Error conectando', err.message));
